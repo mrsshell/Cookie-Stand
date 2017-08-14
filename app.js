02ab26acd30fbf46d6e82fd84aa7ec45a059e235
mrsshell <mrsshell@comcast.net>
2017-08-14 14:50:46 -0700

    Added the contents for the object literals in .js page and started basic HTML structure

---
+++ app.js
@@ -0,0 +1,44 @@
+var store1 = {
+  name: '1st and Pike',
+  min/cust: 23,
+  max/cust: 65,
+  avg-sales: 6.3,
+  hours: [1,2,3,4,5,6,7,8,9,10,11,12]
+
+}
+
+var store2 = {
+  name:'SeaTac Airport',
+  min/cust: 3,
+  max/cust: 11,
+  avg-sales: 20,
+  hours:[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+}
+
+var store3 = {
+  name:'Seattle Center'
+  min/cust:11,
+  max/cust: 38,
+  avg-sales: 3.7,
+  hours:[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+}
+
+var store4 = {
+  name:'Capitol Hill',
+  min/cust: 20,
+  max/cust: 38,
+  avg-sales: 2.3,
+  hours:[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+}
+
+var store5  = {
+  name:'Alkai',
+  min/cust: 2,
+  max/cust: 16,
+  avg-sales: 4.6,
+  hours: [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+}
