193fa7d863ce28a7433aae2c26ca533a0fe301f8
mrsshell <mrsshell@comcast.net>
2017-08-15 10:57:24 -0700

    going through the code in class i am going to review this and make changes and add comments as i go on what i am doing and what it daoes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var store1 = {
   min: 23,
   max: 65,
   averageSales: 6.3,
-  storeHours: ['6:00 am ''7:00 am ', '8:00 am ', '9:00 am ','10:00 am ','11:00 am ','12:00 pm ', '1:00 pm ', '2:00 pm ', '3:00 pm ', '4:00 pm ', '5:00 pm ','6:00 pm ', '7:00 pm '],
+  storeHours: ['6:00 am: ''7:00 am: ', '8:00: am ', '9:00 am: ','10:00: am ','11:00: am ','12:00: pm ', '1:00: pm ', '2:00: pm: ', '3:00: pm ', '4:00: pm ', '5:00: pm ','6:00: pm ', '7:00: pm '],
   hourlySales: [],
   randomCustomers: function() {
     return Math.round(Math.random() * (this.max - this.min) + this.min);
@@ -178,3 +178,64 @@ var store5  = {
 //   newLi.appendChild(img);
 //   list.appendChild(newLi);
 //  }
+var sotreQ ={
+name: '1st and Pike',
+storeHours: [0],
+minCust :0,
+maxCust : 0,
+averageSales: 0;
+simCookies: [],
+randomCustomersPer Hour: function(){
+return Math.round(Math.random()) * (this.maxCust - this. minCust + 1) + this minCust);
+  },
+hourlySales: function(){
+  this.simCookies = [];
+  this.totalCookieSales =0,
+  for (var i = 0 ; i < this.storeHours.length; i++) {
+      this.simCookies.push(Math.round(this.averageSales * this.randomCustomersPer()));
+      var hourlySales = Math.push();
+      this.totalCookieSales += hourlySales;
+    }
+    this.simCookies.push('Total: ')
+    this.
+  },
+
+  dailySalesReport: function(){
+    this.hourlySales();
+    var storeLocation = document.getElementById('list');
+    var locationName = document.createElement('h2');
+    locationName.innerText = this.name;
+    location.appendChild(loactionName);
+    var list = document.getElementById('ul');
+    location.appendChild('list');
+
+    for ()  var i = 0 ; i < this.storeHours.length; i++0) {
+      var listItems = document.createElement('li');
+      listItems.innerText = this.storeHours[i]+ ';'+ this.simCookies[i] + 'cookies';
+
+      list.appendChild(listItems);
+  }
+  listItems.innerText = document.createElement'Total:'
++ this.totalCookieSales + 'cookies';
+
+list.simCookies};
+
+//generate sales
+// declare a variable named list that selectd a ul to fill with data=
+// document.getElementById("list")
+// create a loop
+// set starting and stopping point in iteration
+// create and new variable for new list items
+// change the inner text of new list to be the values of the store hours array
+// append the new list
+storeQ.hourlySales: function();
+var list = document.getElementById('list');
+
+for (var i = 0; i <storeQ.storeHours.length; i++) {
+  var  listItems = document.createElement('li');
+  new.innerText = i;
+  listItems.innerText = storeQ.storeHours[i] + 'storeQ.simCookies[i]' + 'Cookies ';
+  list.appendChild
+  list.appendChild(listItems);
+}
+pike.dailySalesReport();
