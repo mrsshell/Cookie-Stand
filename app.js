70d95e8db2a853a3a0a215d66e06888ca0f205e4
mrsshell <mrsshell@comcast.net>
2017-08-15 14:09:09 -0700

    put in the value data for stores q r and s and removed the old code , cleaned up some commas that should have been senicolens

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ var storeQ ={
   },
   hourlySales: function(){
     this.soldCookies = [];
-    this.totalCookieSales =0,
+    this.totalCookieSales = 0;
     for (var i = 0 ; i < this.storeHours.length; i++) {
       var hourlyCookieSales = Math.round(this.avSales * this.randomCustomersPerHour());
       this.soldCookies.push(hourlyCookieSales);
       this.totalCookieSales += hourlyCookieSales;
     }
   },
-
   dailySalesReport: function(){
     this.hourlySales();
     var storeLocation = document.getElementById('sales-report');
@@ -41,71 +40,98 @@ var storeQ ={
   }
 };
 
-var store1 = {
-  name: '1st and Pike',
-  min: 23,
-  max: 65,
-  averageSales: 6.3,
 
-  hourlySales: [],
-  randomCustomers: function() {
-    return Math.round(Math.random() * (this.max - this.min) + this.min);
-    },
-  salesByHour: function() {
-    var totalSales = 0;
-      for (var i = 0; i < this.storeHours.length; i++) {
-        var cookiesPerHour = Math.round(this.averageSales * this.randomCustomers());
-        this.hourlySales.push(cookiesPerHour);
-        totalSales = (totalSales + cookiesPerHour);
-        this.hourlySales.push(totalSales);
-        this.storeHours.push(' Total ');
+var storeR ={
+  name: 'SeaTac Airport',
+  storeHours: ['6am: ''7am: ', '8am ', '9am: ','10am ','11am ','12pm ', '1pm ', '2pm: ', '3pm ', '4pm ', '5pm ','6pm ', '7pm ','8pm'],
+  minCust :3,
+  maxCust : 11,
+  aveCookies: 20,
+  soldCookies: [],
+  totalCookieSales: 0,
+  randomCustomersPer Hour: function(){
+    return Math.round(Math.random()) * (this.maxCust - this.minCust + 1) + this minCust);
+  },
+  hourlySales: function() {
+    this.soldCookies = [];
+    this.totalCookieSales = 0;
+    for (var i = 0 ; i < this.storeHours.length; i++) {
+      var hourlyCookieSales = Math.round(this.avSales * this.randomCustomersPerHour());
+      this.soldCookies.push(hourlyCookieSales);
+      this.totalCookieSales += hourlyCookieSales;
     }
   },
+  dailySalesReport: function(){
+    this.hourlySales();
+    var storeLocation = document.getElementById('sales-report');
+    var locationName = document.createElement('h2');
+    locationName.innerText = this.name;
+    storeLocation.appendChild(loactionName);
 
-  toSalesPage: function() {
-    for (var i = 0; i < this.hours.length; i++) {
-      var pikeStore = document.getElementById('store1');
-      var newLi = document.createElement('li');
-      var newString = this.hours[i] + this.hourlySales[i] + 'Cookies';
-      newLi.innerText = newString;
-      pikeStore.appendChild(newLi);
+    var list = document.getElementById('ul');
+    location.appendChild('list');
+
+    for (var i = 0 ; i < this.storeHours.length; i++) {
+      var listItems = document.createElement('li');
+      listItems.innerText = this.storeHours[i]+ ':'+ this.soldCookies[i] + 'cookies';
+      list.appendChild(listItems);
     }
-  },
+    var lstItems = document.createElement('li');
+    listItems.innerText = 'Total:' + this.totalCookieSales + 'Cookies';
+    list.appendChild(listItems);
+  }
 };
-store1.salesByHour();
-store1.toSalesPage();
 
 
-var store2 = {
-  name:'SeaTac Airport',
-  min: 3,
-  max: 11,
-  averageSales: 20,
-  hours: ['7:00 am', '8:00 am', '9:00 am','10:00 am','11:00 am','12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm','6:00 pm', '7:00 pm', '8:00 pm'],
-    hourlySales: [],
-    randomCustomers: function() {
-      Math.round(Math.random() * (this.max - this.min) + this.min);
-      },
-      salesByHour: function() {
-        var totalSales = 0;
-          for (var i = 0; i < this.hours.length; i++) {
-            var cookiesPerHour = Math.round(this.averageSales * this.randomCustomers());
-            this.hourlySales.push(cookiesPerHour);
-            totalSales = (totalSales + cookiesPerHour);
-        }
-      },
-      toSalesPage: function() {
-        for (var i = 0; i < this.hours.length; i++) {
-          var pikeStore = document.getElementById('store2');
-          var newLi = document.createElement('li');
-          var newString = this.hours[i] + this.hourlySales[i] + 'Cookies';
-          newLi.innerText = newString;
-          pikeStore.appendChild(newLi);
-        }
-      },
-    };
-    store1.salesByHour();
-    store1.toSalesPage();
+var storeS ={
+  name: '1st and Pike',
+  storeHours: ['6am: ''7am: ', '8am ', '9am: ','10am ','11am ','12pm ', '1pm ', '2pm: ', '3pm ', '4pm ', '5pm ','6pm ', '7pm ','8pm'],
+  minCust : 11,
+  maxCust : 38,
+  aveCookies: 3.7,
+  soldCookies: [],
+  totalCookieSales: 0,
+  randomCustomersPer Hour: function(){
+    return Math.round(Math.random()) * (this.maxCust - this.minCust + 1) + this minCust);
+  },
+  hourlySales: function(){
+    this.soldCookies = [];
+    this.totalCookieSales = 0;
+    for (var i = 0 ; i < this.storeHours.length; i++) {
+      var hourlyCookieSales = Math.round(this.avSales * this.randomCustomersPerHour());
+      this.soldCookies.push(hourlyCookieSales);
+      this.totalCookieSales += hourlyCookieSales;
+    }
+  },
+  dailySalesReport: function(){
+    this.hourlySales();
+    var storeLocation = document.getElementById('sales-report');
+    var locationName = document.createElement('h2');
+    locationName.innerText = this.name;
+    storeLocation.appendChild(loactionName);
+
+    var list = document.getElementById('ul');
+    location.appendChild('list');
+
+    for (var i = 0 ; i < this.storeHours.length; i++) {
+      var listItems = document.createElement('li');
+      listItems.innerText = this.storeHours[i]+ ':'+ this.soldCookies[i] + 'cookies';
+      list.appendChild(listItems);
+    }
+    var lstItems = document.createElement('li');
+    listItems.innerText = 'Total:' + this.totalCookieSales + 'Cookies';
+    list.appendChild(listItems);
+  }
+};
+
+
+
+
+
+
+
+
+
 
 
 var store3 = {
