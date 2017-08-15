ce4016a397153193f82cc370853b426aaff963ea
mrsshell <mrsshell@comcast.net>
2017-08-15 09:10:52 -0700

    push total to store hours array

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var store1 = {
   min: 23,
   max: 65,
   averageSales: 6.3,
-  hours: ['7:00 am', '8:00 am', '9:00 am','10:00 am','11:00 am','12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm','6:00 pm', '7:00 pm', '8:00 pm'],
+  StoreHours: ['6:00 am ''7:00 am ', '8:00 am ', '9:00 am ','10:00 am ','11:00 am ','12:00 pm ', '1:00 pm ', '2:00 pm ', '3:00 pm ', '4:00 pm ', '5:00 pm ','6:00 pm ', '7:00 pm '],
   hourlySales: [],
   randomCustomers: function() {
     return Math.round(Math.random() * (this.max - this.min) + this.min);
@@ -17,6 +17,7 @@ var store1 = {
         this.hourlySales.push(cookiesPerHour);
         totalSales = (totalSales + cookiesPerHour);
         this.hourlySales.push(totalSales);
+        this.hours.push(' Total:');
     }
   },
 
