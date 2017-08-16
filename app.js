0b0fb14c2c5e8ea1e15f9d5ac330d3c9a1cacfde
mrsshell <mrsshell@comcast.net>
2017-08-15 19:16:46 -0700

    added table tags to the html page , table header table data table row, and then built the arra for the object constructor in the js page

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var store = new Store ();
+store.name = ['1st and Pike','SeaTac Airport', 'Seattle Center',
+  'Capitol Hill', 'Alkai'];
+store.hours = ['6am ','7am ','8am ','9am ', '10am ', '11am ','12pm ','1pm ', '2pm ', '3pm ','4pm ', '5pm ','6pm ','7pm ','8pm '];
+store.minCust = [23, 3, 11, 20, 2];
+stotr.maxCust = [];
+store.avCookies = [];
+store.soldCookies = [];
+store.totalCookieSales = [];
+
 var storeQ = {
   name: '1st and Pike',
   storeHours:['6am ','7am ', '8am ', '9am ','10am ','11am ','12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ','6pm ', '7pm ','8pm '],
