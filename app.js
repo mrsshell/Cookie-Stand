a2b6ad6f99b9bc1813d90b0b386b525b4129023e
mrsshell <mrsshell@comcast.net>
2017-08-16 17:01:52 -0700

    made the constructor the way they have it in the book ans created new new instances of all of the new stores.

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 'use strict';
 
+var hours = ['6am ','7am ','8am ','9am ', '10am ', '11am ','12pm ','1pm ', '2pm ', '3pm ','4pm ', '5pm ','6pm ','7pm ','8pm '];
+var totalAllStores = 0;
+
 function Store (name, hours,soldCookies, totalCookieSales){
-this.name = [,'1st and Pike','SeaTac Airport', 'Seattle Center',
-  'Capitol Hill', 'Alkai'];
-this.hours = ['6am ','7am ','8am ','9am ', '10am ', '11am ','12pm ','1pm ', '2pm ', '3pm ','4pm ', '5pm ','6pm ','7pm ','8pm '];
-this.minCust = [,23, 3, 11, 20, 2];
-this.maxCust = [65, 24, 38, 38, 16];
-this.avCookies = [6.3, 1.2, 3.7, 2.3, 4.6];
-this.soldCookies = [],
-this.totalCookieSales = 0,
+this.name = name;
+this.minCust = minCust;
+this.maxCust = maxCust;
+this.avCookies = avCookies;
+this.soldCookies = [];
+this.totalCookieSales = 0;
 
 this.hourlySales = function(){
   this.soldCookies = [];
   this.totalCookieSales = 0;
   randomCustomersPerHour = function() {
-    for (var i = 0 ; i < this.storeHours.length ; i++) {
+    for (var i = 0 ; i < hours.length ; i++) {
       var hourlyCookieSales = Math.round(this.avCookies * this.randomCustomersPerHour());
       this.soldCookies.push(hourlyCookieSales);
       this.totalCookieSales += hourlyCookieSales;
@@ -35,17 +36,16 @@ this.dailySalesReport = function(){
       listItems.innerText = this.storeHours[i] + ':' + this.soldCookies[i] + ' cookies';
       list.appendChild(listItems);
     }
-    // var listItems = document.createElement('li');
-    // listItems.innerText = 'Total:' + this.totalCookieSales + ' cookies';
-    // list.appendChild(listItems);
+    var listItems = document.createElement('li');
+    listItems.innerText = 'Total:' + this.totalCookieSales + ' cookies';
+     list.appendChild(listItems);
   };
 };
-
-var stores = new Store (name, soldCookies, totalCookieSales);{
- for (var i = 0; i < this.storeHours.length; i++) {
- store.push(new Stores(this.name[i], this.soldCookies[i], this.totalCookieSales[i])
-  }
-};
+var stAndPike = new Store('1st and Pike',23 ,65 ,6.3);
+var SeaTac = new Store('SeaTac Airport', 3 ,24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11 , 38 , 3.7);
+var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+var Alki = new Store('Alki', 2, 16, 4.6);
 
 // var storeQ = {
 //   name: '1st and Pike',
